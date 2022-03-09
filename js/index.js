e50a3e1446fbcaabeff92a34468c200b2376fa7e
Sainath Poojary <spoojary614@gmail.com>
2022-03-09 14:35:41 +0530

    Fixed not working of Dark theme in resource.html

--- js/index.js
+++ js/index.js
@@ -1,39 +1,33 @@
-
-
-let category = document.getElementsByClassName('category');
+let category = document.getElementsByClassName("category");
 
 const selectBtn = document.querySelectorAll(".btn-style");
 const contributionBtn = document.querySelector(".contribution-btn");
 
 for (let i = 0; i < category.length; i++) {
+  category[i].querySelector(".btn-style").addEventListener("click", () => {
+    contributionBtn.style.backgroundColor = "white";
+    contributionBtn.style.color = "black";
+    for (let j = 0; j < category.length; j++) {
+      selectBtn[j].style.backgroundColor = "white";
+      selectBtn[j].style.color = "black";
+    }
+    selectBtn[i].style.backgroundColor = "#6940d3";
+    selectBtn[i].style.color = "white";
 
-    category[i].querySelector('.btn-style').addEventListener('click', () => {
+    for (let i = 0; i < category.length; i++) {
+      let x = category[i].querySelector(".select-option");
+      x.style.display = "none";
+    }
 
-        contributionBtn.style.backgroundColor = "white";
-        contributionBtn.style.color = "black";
-        for (let j = 0; j < category.length; j++) {
-            selectBtn[j].style.backgroundColor = "white";
-            selectBtn[j].style.color = "black";
-        }
-        selectBtn[i].style.backgroundColor = "#6940d3";
-        selectBtn[i].style.color = "white";
-
-        for (let i = 0; i < category.length; i++) {
-            let x = category[i].querySelector('.select-option');
-            x.style.display = "none";
-        }
-
-        let x = category[i].querySelector('.select-option');
-        x.style.display = "block";
-
-    })
+    let x = category[i].querySelector(".select-option");
+    x.style.display = "block";
+  });
 }
 
 const Body = document.getElementsByTagName("body");
 const para = document.getElementById("mainParagraph");
 const List = document.querySelectorAll("#contactList > li");
-const icon1 = document.getElementById("darkSwitch1");
-const icon2 = document.getElementById("darkSwitch1");
+const icon = document.getElementById("darkSwitch");
 const sidebar = document.getElementById("sidebar");
 const List2 = document.querySelectorAll("button.link-dark");
 const text = document.getElementById("categoryText");
@@ -41,90 +35,79 @@ const hr = document.getElementsByClassName("catHr");
 const head = document.getElementById("contriHead");
 const List3 = document.querySelectorAll("li.font");
 const $points = document.querySelectorAll("div.points");
-console.log(List2);
 
 function dark_mode() {
-    var count = 0;
-    if (window.location.href.includes("index.html")) {
-        icon1.classList.toggle("fa-sun");
-        Body[0].classList.toggle("dark");
-        para.classList.toggle("dark-para");
-        for (i = 0; i < List.length; i++) {
-            List[i].classList.toggle("dark-list");
-        }
-        for (i = 0; i < icon1.classList.length; i++) {
-            if (icon1.classList[i] == "fa-sun") {
-                count++;
-                break;
-            }
-        }
+  var count = 0;
+  if (window.location.href.includes("index.html")) {
+    icon.classList.toggle("fa-sun");
+    Body[0].classList.toggle("dark");
+    para.classList.toggle("dark-para");
+    for (i = 0; i < List.length; i++) {
+      List[i].classList.toggle("dark-list");
     }
-    else {
-        icon2.classList.toggle("fa-sun");
-        Body[0].classList.toggle("dark");
-        sidebar.classList.toggle("dark-side-bar");
-        for (i = 0; i < List2.length; i++) {
-            List2[i].classList.toggle("dark-list-2");
-        }
-        for (i = 0; i < hr.length; i++) {
-            hr[i].classList.toggle("dark-hr");
-        }
-        text.classList.toggle("dark-text");
-        head.classList.toggle("dark-head");
-        for (i = 0; i < List3.length; i++) {
-            List3[i].classList.toggle("dark-list-3");
-        }
-        for (i = 0; i < $points.length; i++) {
-            $points[i].classList.toggle("dark-points");
-        }
-        for (i = 0; i < icon2.classList.length; i++) {
-            if (icon2.classList[i] == "fa-sun") {
-                count++;
-                break;
-            }
-        }
+    for (i = 0; i < icon.classList.length; i++) {
+      if (icon.classList[i] == "fa-sun") {
+        count++;
+        break;
+      }
     }
+  } else {
+    icon.classList.toggle("fa-sun");
+    Body[0].classList.toggle("dark");
+    sidebar.classList.toggle("dark-side-bar");
+    for (i = 0; i < List2.length; i++) {
+      List2[i].classList.toggle("dark-list-2");
+    }
+    for (i = 0; i < hr.length; i++) {
+      hr[i].classList.toggle("dark-hr");
+    }
+    text.classList.toggle("dark-text");
+    head.classList.toggle("dark-head");
+    for (i = 0; i < List3.length; i++) {
+      List3[i].classList.toggle("dark-list-3");
+    }
+    for (i = 0; i < $points.length; i++) {
+      $points[i].classList.toggle("dark-points");
+    }
+    for (i = 0; i < icon.classList.length; i++) {
+      if (icon.classList[i] == "fa-sun") {
+        count++;
+        break;
+      }
+    }
+  }
 
-    if (count) {
-        sessionStorage.setItem("switch", 1);
-        console.log("yes");
-    }
-    else {
-        sessionStorage.setItem("switch", 0);
-        console.log("no");
-    }
+  if (count) {
+    sessionStorage.setItem("switch", 1);
+  } else {
+    sessionStorage.setItem("switch", 0);
+  }
 }
 if (Number(sessionStorage.getItem("switch"))) {
-    if (window.location.href.includes("index.html")) {
-        icon1.classList.toggle("fa-sun");
-        Body[0].classList.toggle("dark");
-        para.classList.toggle("dark-para");
-        for (i = 0; i < List.length; i++) {
-            List[i].classList.toggle("dark-list");
-        }
+  if (window.location.href.includes("index.html")) {
+    icon.classList.toggle("fa-sun");
+    Body[0].classList.toggle("dark");
+    para.classList.toggle("dark-para");
+    for (i = 0; i < List.length; i++) {
+      List[i].classList.toggle("dark-list");
     }
-    else {
-        icon2.classList.toggle("fa-sun");
-        Body[0].classList.toggle("dark");
-        sidebar.classList.toggle("dark-side-bar");
-        for (i = 0; i < List2.length; i++) {
-            List2[i].classList.toggle("dark-list-2");
-        }
-        text.classList.toggle("dark-text");
-        for (i = 0; i < hr.length; i++) {
-            hr[i].classList.toggle("dark-hr");
-        }
-        head.classList.toggle("dark-head");
-        for (i = 0; i < List3.length; i++) {
-            List3[i].classList.toggle("dark-list-3");
-        }
-        for (i = 0; i < $points.length; i++) {
-            $points[i].classList.toggle("dark-points");
-        }
+  } else {
+    icon.classList.toggle("fa-sun");
+    Body[0].classList.toggle("dark");
+    sidebar.classList.toggle("dark-side-bar");
+    for (i = 0; i < List2.length; i++) {
+      List2[i].classList.toggle("dark-list-2");
     }
+    text.classList.toggle("dark-text");
+    for (i = 0; i < hr.length; i++) {
+      hr[i].classList.toggle("dark-hr");
+    }
+    head.classList.toggle("dark-head");
+    for (i = 0; i < List3.length; i++) {
+      List3[i].classList.toggle("dark-list-3");
+    }
+    for (i = 0; i < $points.length; i++) {
+      $points[i].classList.toggle("dark-points");
+    }
+  }
 }
-
-
-
-
-
